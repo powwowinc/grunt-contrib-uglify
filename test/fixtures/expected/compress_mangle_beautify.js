c92083b5cf72b42e83d3698db620c1b9df99b103
Alexey Vinokurov <alwinn1977@gmail.com>
2017-12-27 17:28:39 +0400

    Revert "upgrade to uglify-js 3.3.0"
    This reverts commit 6db68bd

--- test/fixtures/expected/compress_mangle_beautify.js
+++ test/fixtures/expected/compress_mangle_beautify.js
@@ -1,8 +1,6 @@
-var longNameA = 1, longNameB = 2;
-
 function longFunctionC(n, o) {
     return longNameA + longNameB + n + o;
 }
 
-var result = longFunctionC(3, 4);
+var longNameA = 1, longNameB = 2, result = longFunctionC(3, 4);
 // This is a footer.
--- test/fixtures/expected/compress_mangle_except.js
+++ test/fixtures/expected/compress_mangle_except.js
@@ -1,1 +1,1 @@
-var longNameA=1,longNameB=2;function longFunctionC(argumentC,n){return longNameA+longNameB+argumentC+n}var result=longFunctionC(3,4);
+function longFunctionC(argumentC,n){return longNameA+longNameB+argumentC+n}var longNameA=1,longNameB=2,result=longFunctionC(3,4);
